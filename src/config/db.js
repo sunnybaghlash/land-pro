9a973241713f11be1781f6d21fb04457feff9a35
sunny baghla <sunnybaghla@rocketmail.com>
2026-08-31 18:32:29 +0530

    update db

--- src/config/db.js
+++ src/config/db.js
@@ -1,18 +1,37 @@
-const {Pool}=require("pg")
+// const {Pool}=require("pg")
+// require("dotenv").config({ override: true });
+
+// const pool= new Pool({
+//     host:process.env.DB_HOST,
+//     port:process.env.DB_PORT,
+//     user:process.env.DB_USER,
+//     password:process.env.DB_PASSWORD,
+//     database:process.env.DB_NAME
+// });
+// pool.on("connect",()=>{
+//     console.log("postges database is connected");
+
+// })
+// pool.on("error",(err)=>{
+//     console.log(`err is ${err}`)
+// })
+// module.exports=pool
+const { Pool } = require("pg");
 require("dotenv").config({ override: true });
 
-const pool= new Pool({
-    host:process.env.DB_HOST,
-    port:process.env.DB_PORT,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
-pool.on("connect",()=>{
-    console.log("postges database is connected");
 
-})
-pool.on("error",(err)=>{
-    console.log(`err is ${err}`)
-})
-module.exports=pool
+pool.on("connect", () => {
+    console.log("postgres database is connected");
+});
+
+pool.on("error", (err) => {
+    console.log(`db error: ${err.message}`);
+});
+
+module.exports = pool;
