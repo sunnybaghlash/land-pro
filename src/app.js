9e835295353ca14d77faf0713c1e1de278355cc3
sunny baghla <sunnybaghla@rocketmail.com>
2026-08-31 18:44:40 +0530

    app.js

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ app.use(cors())
 app.use(express.json())
 app.use("/api/auth",authRoute)
 app.get("/",(req,res)=>{
+    res.status(200).json({message:"hello"})
     console.log("Hello World")
 })
 module.exports=app;
